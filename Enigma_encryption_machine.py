1fc53d77d1b67cb7b5b97b08fae3365166468f63
NikitaBelqa <Nik.belyakovich@yandex.ru>
2025-12-14 04:58:40 +0300

    Added a few coctails

--- Enigma_encryption_machine.py
+++ Enigma_encryption_machine.py
@@ -2,12 +2,15 @@ import random
 alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./"
 message = "lambda"#input("Your message: ").lower()
 
+for i in message:
+    print(f"Индекс буквы {i.upper()} - {alphabet.find(i)} \nASCII код - {ord(i)}\nИндекс + ASCII код - {alphabet.find(i) + ord(i)}\nИтоговая кодировка {chr(alphabet.find(i) + ord(i))} - {alphabet[(alphabet.find(i) + ord(i)) % len(alphabet)]}\n")
+
 def encrypt(word):
-    return "".join([alphabet[alphabet.find(i) + 1] for i in word])
+    return "".join([alphabet[(alphabet.find(i) + ord(i)) % len(alphabet)] for i in word])
 
 def decrypt(word):
-    return "".join([alphabet[alphabet.find(i) - 1] for i in word])
+    return "".join([alphabet[(ord(i) - alphabet.find(i))] for i in word])
 
-new_message = encrypt(message)
-print(new_message)
-print(decrypt(new_message))
+# new_message = encrypt(message)
+# print(new_message)
+# print(decrypt(new_message))
--- Home_bar-helper.py
+++ Home_bar-helper.py
@@ -1,11 +1,11 @@
 import random
-alcohol_bar = ["Teqilla", "Rum", "Vodka", "Gin"]
-non_alco_bar = ["Cola", "Lemon juice", "Tripple sec", "Tonic", "Orange juice", "Sugar syrup", "Lemon-lime soda", "Blue curacao", "Grenadine"]
+alcohol_bar = ["Teqilla", "Rum", "Vodka", "Gin", "Champangine", "Tripple sec", "Aperol"]
+non_alco_bar = ["Cola", "Lemon juice", "Tonic", "Orange juice", "Sugar syrup", "Lemon-lime soda", "Blue curacao", "Grenadine", "Peach concentrate", "Mint"]
 bar = alcohol_bar + non_alco_bar
 
 
 coctails = {"Long island": ["Teqilla", "Rum", "Vodka", "Gin", "Tripple sec", "Cola"],
-            #"Whiskey cola": ["Whiskey", "Cola"],
+            "Mojito": ["Rum", "Soda", "Lemon juice", "Sugar syrup", "Mint"],
             "Cuba libre": ["Rum", "Cola"],
             "Gin tonic": ["Gin", "Tonic"],
             "Margarita": ["Teqilla", "Tripple sec"],
@@ -13,7 +13,13 @@ coctails = {"Long island": ["Teqilla", "Rum", "Vodka", "Gin", "Tripple sec", "Co
             "Gin sour": ["Gin", "Lemon juice", "Sugar syrup"],
             "Daiqiri": ["Rum", "Lime juice", "Sugar syrup"],
             "Electric lemonade": ["Vodka", "Lemon juice", "Sugar syrup", "Lemon-lime soda", "Blue curacao"],
-            "Teqilla sunrise": ["Teqilla", "Grenadine", "Orange juice"]}
+            "Teqilla sunrise": ["Teqilla", "Grenadine", "Orange juice"],
+            "Bellini": ["Champangine", "Peach concentrate"],
+            "Mimosa": ["Champangine", "Orange juice"], 
+            "French 75": ["Champangine", "Gin", "Lemon juice", "Sugar syrup"],
+            "Aperol Spritz": ["Champangine", "Aperol"],
+            "Northern lights": ["Champangine", "Vodka", "Lemon juice", "Sugar syrup"],
+            "Cosmopolitan": ["Vodka", "Tripple sec", "Cranberry juice", "Lemon juice"]}
 
 def get_coctail(val):
     for key, value in coctails.items():
@@ -26,16 +32,20 @@ def in_coctails(val):
 
 
 def run(inp_bar):
-    print("Input values:", list(set(inp_bar)))
+    print("Input values:", list(set(inp_bar)), "\n")
+    #total_enough = []
+    #total_not_enough = []
     for recepie in coctails.values():
         remain = list(set(recepie) - set(inp_bar))
         if (sorted(remain) != sorted(recepie)):
             if not remain:
+                #total_enough.append(f"You got everything for {get_coctail(recepie)}.\n")
                 print(f"You got everything for {get_coctail(recepie)}.")
             else:
                 remain = ", ".join(remain)
+                #total_not_enough.append(f"For {get_coctail(recepie)} not enough {remain}.\n")
                 print(f"For {get_coctail(recepie)} not enough {remain}.")
-
+    #print(total_enough, total_not_enough)
     print("")
 
 while True:
